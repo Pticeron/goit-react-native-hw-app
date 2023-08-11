cdd2fa7e044b7717eb52feebfe75deba349da1ea
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-08-11 07:29:23 +0300

    create project

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { useFonts } from "expo-font";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet, Text, View } from "react-native";
+
+import RegistrationScreen from './src/Screens/RegistrationScreen';
+import LoginScreen from './src/Screens/LoginScreen';
+import Home from './src/Screens/Home';
+import PostsScreen from './src/Screens/PostsScreen';
+import MapScreen from './src/Screens/MapScreen';
+import CreatePostsScreen from './src/Screens/CreatePostsScreen';
+import CommentsScreen from './src/Screens/CommentsScreen';
+import ProfileScreen from './src/Screens/ProfileScreen';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./src/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./src/fonts/Roboto-Bold.ttf"),
+    "Roboto-Medium": require("./src/fonts/Roboto-Medium.ttf"),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  const MainStack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MainStack.Navigator initialRouteName="RegistrationScreen">
+        <MainStack.Screen
+          name="Registration"
+          component={RegistrationScreen}
+          options={{ headerShown: false }}
+        />
+        <MainStack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <MainStack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <MainStack.Screen name="PostsScreen" component={PostsScreen} />
+        <MainStack.Screen name="Map" component={MapScreen} />
+        <MainStack.Screen
+          name="CreatePosts"
+          component={CreatePostsScreen}
+          options={{ headerShown: false }}
+        />
+        <MainStack.Screen name="Comments" component={CommentsScreen} />
+        <MainStack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ headerShown: false }}
+        />
+      </MainStack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
